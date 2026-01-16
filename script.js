d5e19f30fd316ab2ef96f93f029d6e4f6dcf6b7b
Shree's <shreyaaa.153@gmail.com>
2026-01-16 17:34:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Dont say no, please!",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...🥺🥺",
+    "Ok fine, I will stop asking...😏",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    const song = document.getElementById("valentineSong");
+    song.play(); // ✅ allowed (user clicked)
+
+    document.querySelector(".container").innerHTML = `
+        <h1>Knew you would say yes! 💖</h1>
+        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmo3c3l5ODh3ZGN6NHhhaDE2Mjg1ZjkwOXczdDFxbWM3dTBtaW9zaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/9XY4f3FgFTT4QlaYqa/giphy.gif">
+    `;
+}
